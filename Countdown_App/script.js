b56fa6aae118e5e43721a0042c765c8b13164530
FranciscoPorto-dev <franmed1276@gmail.com>
2024-02-02 15:18:00 -0500

    Adding a new project which is a countdown tiemr

---
+++ Countdown_App/script.js
@@ -0,0 +1,10 @@
+var dayEl = document.getElementById("days");
+var hourEl = document.getElementById("hours");
+var minuteEl = document.getElementById("minutes");
+var secondsEl = document.getElementById("seconds");
+var inputEl = document.getElementById("input")
+
+
+function countdown() {
+    
+}
